d0adcebd543f22aa78ea6554c0629f1fa233d061
Angger Prayugo <anggerprayugo251@gmail.com>
2022-09-16 14:05:32 +0800

    created tile for main board

--- sudoku.js
+++ sudoku.js
@@ -42,4 +42,14 @@ function setGame() {
       number.classList.add("number");
       document.getElementById("digits").appendChild(number);
   }
+
+  // Board 9x9
+  for (let r = 0; r < 9; r++) {
+    for (let c = 0; c < 9; c++) {
+        let tile = document.createElement("div");
+        tile.id = r.toString() + "-" + c.toString();
+        tile.classList.add("tile");
+        document.getElementById("board").append(tile);
+    }
+  }
 }
