b345d5ea4ff1b20ea35bfce54255a63ba41851f8
Angger Prayugo <anggerprayugo251@gmail.com>
2022-09-18 11:46:39 +0800

    finishing the game

--- sudoku.js
+++ sudoku.js
@@ -1,59 +1,96 @@
+
 var numSelected = null;
 var tileSelected = null;
 
 var errors = 0;
 
 var board = [
-    "9--2--6--",
-    "-2--4--8-",
-    "--7--9--3",
-    "6--3--8--",
-    "-5--9--1-",
-    "--9--1--2",
-    "2--8--9--",
-    "-7--1--3-",
-    "1--6--2--"
+  "9--2--6--",
+  "-2--4--8-",
+  "--7--9--3",
+  "6--3--8--",
+  "-5--9--1-",
+  "--9--1--2",
+  "2--8--9--",
+  "-7--1--3-",
+  "1--6--2--"
 ]
 
 var solution = [
-    "918273645",
-    "523146789",
-    "467589123",
-    "641325897",
-    "352798416",
-    "789461352",
-    "234857961",
-    "876912534",
-    "195634278"
+  "918273645",
+  "523146789",
+  "467589123",
+  "641325897",
+  "352798416",
+  "789461352",
+  "234857961",
+  "876912534",
+  "195634278"
 ]
 
 window.onload = function() {
-  setGame();
+    setGame();
 }
 
 function setGame() {
-  // Digits 1-9
-  for (let i = 1; i <= 9; i++) {
-      let number = document.createElement("div");
-      number.id = i
-      number.innerText = i;
-      number.addEventListener("click", selectNumber);
-      number.classList.add("number");
-      document.getElementById("digits").appendChild(number);
-  }
-
-  // Board 9x9
-  for (let r = 0; r < 9; r++) {
-    for (let c = 0; c < 9; c++) {
-        let tile = document.createElement("div");
-        tile.id = r.toString() + "-" + c.toString();
-        tile.classList.add("tile");
-        document.getElementById("board").append(tile);
+    // Digits 1-9
+    for (let i = 1; i <= 9; i++) {
+        //<div id="1" class="number">1</div>
+        let number = document.createElement("div");
+        number.id = i
+        number.innerText = i;
+        number.addEventListener("click", selectNumber);
+        number.classList.add("number");
+        document.getElementById("digits").appendChild(number);
     }
-  }
+
+    // Board 9x9
+    for (let r = 0; r < 9; r++) {
+        for (let c = 0; c < 9; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            if (board[r][c] != "-") {
+                tile.innerText = board[r][c];
+                tile.classList.add("tile-start");
+            }
+            if (r == 2 || r == 5) {
+                tile.classList.add("horizontal-line");
+            }
+            if (c == 2 || c == 5) {
+                tile.classList.add("vertical-line");
+            }
+            tile.addEventListener("click", selectTile);
+            tile.classList.add("tile");
+            document.getElementById("board").append(tile);
+        }
+    }
 }
 
 function selectNumber(){
-  numSelected = this;
-  numSelected.classList.add("number-selected");
+    if (numSelected != null) {
+        numSelected.classList.remove("number-selected");
+    }
+    numSelected = this;
+    numSelected.classList.add("number-selected");
+}
+
+function selectTile() {
+    if (numSelected) {
+        if (this.innerText != "") {
+            return;
+        }
+
+        // "0-0" "0-1" .. "3-1"
+        let coords = this.id.split("-"); //["0", "0"]
+        let r = parseInt(coords[0]);
+        let c = parseInt(coords[1]);
+
+        if (solution[r][c] == numSelected.id) {
+            this.innerText = numSelected.id;
+        }
+        else {
+            errors += 1;
+            document.getElementById("errors").innerText = errors;
+        }
+    }
 }
