9f8d71dbc084d9998e1cb2f63ec41bae84914ac6
Angger Prayugo <anggerprayugo251@gmail.com>
2022-09-18 10:28:59 +0800

    adding background color to selected number

--- sudoku.js
+++ sudoku.js
@@ -34,11 +34,10 @@ window.onload = function() {
 function setGame() {
   // Digits 1-9
   for (let i = 1; i <= 9; i++) {
-      //<div id="1" class="number">1</div>
       let number = document.createElement("div");
       number.id = i
       number.innerText = i;
-      // number.addEventListener("click", selectNumber);
+      number.addEventListener("click", selectNumber);
       number.classList.add("number");
       document.getElementById("digits").appendChild(number);
   }
@@ -52,4 +51,9 @@ function setGame() {
         document.getElementById("board").append(tile);
     }
   }
+}
+
+function selectNumber(){
+  numSelected = this;
+  numSelected.classList.add("number-selected");
 }
